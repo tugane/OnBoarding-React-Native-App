4e5541984398f55991be502947826392d6e006e3
Tugane Amiel <69383438+tugane@users.noreply.github.com>
2022-12-24 22:01:20 +0200

    Welcome Screen

--- screens/WelcomeScreen.tsx
+++ screens/WelcomeScreen.tsx
@@ -1,19 +1,125 @@
-import { Text, View } from "react-native";
+import {
+  Dimensions,
+  ImageBackground,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
+import Spacing from "../constants/Spacing";
 import FontSize from "../constants/FontSize";
+import Colors from "../constants/Colors";
+import Font from "../constants/Font";
+import { NativeStackScreenProps } from "@react-navigation/native-stack";
+import { RootStackParamList } from "../types";
+const { height } = Dimensions.get("window");
 
-const Welcome: React.FC = () => {
+type Props = NativeStackScreenProps<RootStackParamList, "Welcome">;
+
+const WelcomeScreen: React.FC<Props> = ({ navigation: { navigate } }) => {
   return (
-    <View>
-      <Text
-        style={{
-          fontSize: FontSize.small,
-        }}
-      >
-        Welcome
-      </Text>
-    </View>
+    <SafeAreaView>
+      <View>
+        <ImageBackground
+          style={{
+            height: height / 2.5,
+          }}
+          resizeMode="contain"
+          source={require("../assets/images/welcome-img.png")}
+        />
+        <View
+          style={{
+            paddingHorizontal: Spacing * 4,
+            paddingTop: Spacing * 4,
+          }}
+        >
+          <Text
+            style={{
+              fontSize: FontSize.xxLarge,
+              color: Colors.primary,
+              fontFamily: Font["poppins-bold"],
+              textAlign: "center",
+            }}
+          >
+            Discover Your Dream Job here
+          </Text>
+
+          <Text
+            style={{
+              fontSize: FontSize.small,
+              color: Colors.text,
+              fontFamily: Font["poppins-regular"],
+              textAlign: "center",
+              marginTop: Spacing * 2,
+            }}
+          >
+            Explore all the existing job roles based or your interest and study
+            major
+          </Text>
+        </View>
+        <View
+          style={{
+            paddingHorizontal: Spacing * 2,
+            paddingTop: Spacing * 6,
+            flexDirection: "row",
+          }}
+        >
+          <TouchableOpacity
+            onPress={() => navigate("Login")}
+            style={{
+              backgroundColor: Colors.primary,
+              paddingVertical: Spacing * 1.5,
+              paddingHorizontal: Spacing * 2,
+              width: "48%",
+              borderRadius: Spacing,
+              shadowColor: Colors.primary,
+              shadowOffset: {
+                width: 0,
+                height: Spacing,
+              },
+              shadowOpacity: 0.3,
+              shadowRadius: Spacing,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: Font["poppins-bold"],
+                color: Colors.onPrimary,
+                fontSize: FontSize.large,
+                textAlign: "center",
+              }}
+            >
+              Login
+            </Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            onPress={() => navigate("Register")}
+            style={{
+              paddingVertical: Spacing * 1.5,
+              paddingHorizontal: Spacing * 2,
+              width: "48%",
+              borderRadius: Spacing,
+            }}
+          >
+            <Text
+              style={{
+                fontFamily: Font["poppins-bold"],
+                color: Colors.text,
+                fontSize: FontSize.large,
+                textAlign: "center",
+              }}
+            >
+              Register
+            </Text>
+          </TouchableOpacity>
+        </View>
+      </View>
+    </SafeAreaView>
   );
 };
 
-export default Welcome;
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({});
